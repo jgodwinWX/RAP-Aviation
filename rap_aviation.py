c2acc961cf4dcfd4560d56d969428b4f0f1c1cfe
jgodwinWX <jasonwgodwin@gmail.com>
2018-07-26 16:11:37 -0500

    Divergence Tendency

--- rap_aviation.py
+++ rap_aviation.py
@@ -34,6 +34,9 @@ def contourPlot(var1,var2,lev,title,save):
     elif save == 'ri':
         values = numpy.arange(0,1,0.1)
         fmat = '%.1f'
+    elif save == 'dt':
+        values = numpy.arange(5,100,5)
+        fmat = '%.0f'
     else:
         values = numpy.arange(numpy.nanmin(var2),numpy.nanmax(var2),(numpy.nanmax(var2)-numpy.nanmin(var2))/10.0)
         fmat = '%f'
@@ -134,6 +137,44 @@ def potentialTemperature(t,p):
         theta[i,:,:] = t[i,:,:] * (1000.0 / p[i])**KAPPA
     return theta
 
+def importVars(grib):
+    uwind = []
+    uwind_pres = []
+    vwind = []
+    vwind_pres = []
+    hght = []
+    hght_pres = []
+    temp = []
+    temp_pres = []
+
+    for grb in grib:
+        if grb['typeOfLevel'] == 'isobaricInhPa' and grb['name'] == 'U component of wind':
+            uwind.append(grb.values)
+            uwind_pres.append(grb['level'])
+        elif grb['typeOfLevel'] == 'isobaricInhPa' and grb['name'] == 'V component of wind':
+            vwind.append(grb.values)
+            vwind_pres.append(grb['level'])
+        elif grb['typeOfLevel'] == 'isobaricInhPa' and grb['name'] == 'Geopotential Height':
+            hght.append(grb.values)
+            hght_pres.append(grb['level'])
+        elif grb['typeOfLevel'] == 'isobaricInhPa' and grb['name'] == 'Temperature':
+            temp.append(grb.values)
+            temp_pres.append(grb['level'])
+        else:
+            continue
+    lats,lons = grb.latlons()
+    return uwind,uwind_pres,vwind,vwind_pres,hght,hght_pres,temp,temp_pres,lats,lons
+
+# check the pressure levels
+def pressureCheck(var1,var2,var3,var4):
+    assert (var1 == var2 == var3 == var4),"Pressure levels must be the same!"
+
+# check the shapes
+def shapeCheck(var1,var2,var3,var4,la,lo):
+    assert (var1.shape == var2.shape == var3.shape == var4.shape),"All fields must be the same shape!"
+    assert (var1[0,:,:].shape == la.shape),"Latitude does not match shape of vector fields!"
+    assert (var1[0,:,:].shape == lo.shape),"Longitude does not match shape of vector fields!"
+
 # constants
 MPS_TO_KT = 1.94384     # meters per second to knots conversion factor
 sigma = 3               # smoothing factor used by Gaussian Filter
@@ -141,70 +182,58 @@ debug = False           # set this to "True" to plot out just one map
 
 # open the grib file
 rap = pygrib.open('rap/latest_rap.grib2')
+fcst = pygrib.open('rap/rap_fcst03.grib2')
 
-# loop through the messages and get the height, u wind, and v wind messages
-uwind = []
-uwind_pres = []
-vwind = []
-vwind_pres = []
-hght = []
-hght_pres = []
-temp = []
-temp_pres = []
+uwind_now,uwind_pres_now,vwind_now,vwind_pres_now,hght_now,hght_pres_now,temp_now,temp_pres_now,lats,lons = importVars(rap)
+uwind_fcst,uwind_pres_fcst,vwind_fcst,vwind_pres_fcst,hght_fcst,hght_pres_fcst,temp_fcst,temp_pres_fcst,lats_fcst,lons_fcst = importVars(fcst)
 
-for grb in rap:
-    if grb['typeOfLevel'] == 'isobaricInhPa' and grb['name'] == 'U component of wind':
-        uwind.append(grb.values)
-        uwind_pres.append(grb['level'])
-    elif grb['typeOfLevel'] == 'isobaricInhPa' and grb['name'] == 'V component of wind':
-        vwind.append(grb.values)
-        vwind_pres.append(grb['level'])
-    elif grb['typeOfLevel'] == 'isobaricInhPa' and grb['name'] == 'Geopotential Height':
-        hght.append(grb.values)
-        hght_pres.append(grb['level'])
-    elif grb['typeOfLevel'] == 'isobaricInhPa' and grb['name'] == 'Temperature':
-        temp.append(grb.values)
-        temp_pres.append(grb['level'])
-    else:
-        continue
-
-# get the latitude and longitude values
-lats,lons = grb.latlons()
-
-# check to make sure the pressure levels are lined up...they should be, but make sure!
-assert (uwind_pres == vwind_pres == hght_pres == temp_pres),"Pressure levels must be the same!"
+# make sure the pressure levels line up
+pressureCheck(uwind_pres_now,vwind_pres_now,hght_pres_now,temp_pres_now)
+pressureCheck(uwind_pres_fcst,vwind_pres_fcst,hght_pres_fcst,temp_pres_fcst)
 
 # convert everything into numpy arrays and do some more checks
-uwind = numpy.array(uwind)
-vwind = numpy.array(vwind)
-hght = numpy.array(hght)
-temp = numpy.array(temp)
-assert (uwind.shape == vwind.shape == hght.shape == temp.shape),"All fields must be the same shape!"
-assert (uwind[0,:,:].shape == lats.shape),"Latitude does not match shape of vector fields!"
-assert (uwind[0,:,:].shape == lons.shape),"Longitude does not match shape of vector fields!"
+uwind_now = numpy.array(uwind_now)
+vwind_now = numpy.array(vwind_now)
+hght_now = numpy.array(hght_now)
+temp_now = numpy.array(temp_now)
+uwind_fcst = numpy.array(uwind_fcst)
+vwind_fcst = numpy.array(vwind_fcst)
+hght_fcst = numpy.array(hght_fcst)
+temp_fcst = numpy.array(temp_fcst)
+
+# check the shapes of everything
+shapeCheck(uwind_now,vwind_now,hght_now,temp_now,lats,lons)
+shapeCheck(uwind_fcst,vwind_fcst,hght_fcst,temp_fcst,lats,lons)
 
 # run the base variables through a gaussian filter to smooth the output
-for i in range(uwind.shape[0]):
-    uwind[i,:,:] = gfilter(uwind[i,:,:],sigma)
-    vwind[i,:,:] = gfilter(vwind[i,:,:],sigma)
-    hght[i,:,:] = gfilter(hght[i,:,:],sigma)
-    temp[i,:,:] = gfilter(temp[i,:,:],sigma)
+for i in range(uwind_now.shape[0]):
+    uwind_now[i,:,:] = gfilter(uwind_now[i,:,:],sigma)
+    vwind_now[i,:,:] = gfilter(vwind_now[i,:,:],sigma)
+    hght_now[i,:,:] = gfilter(hght_now[i,:,:],sigma)
+    temp_now[i,:,:] = gfilter(temp_now[i,:,:],sigma)
+    uwind_fcst[i,:,:] = gfilter(uwind_fcst[i,:,:],sigma)
+    vwind_fcst[i,:,:] = gfilter(vwind_fcst[i,:,:],sigma)
+    hght_fcst[i,:,:] = gfilter(hght_fcst[i,:,:],sigma)
+    temp_fcst[i,:,:] = gfilter(temp_fcst[i,:,:],sigma)
 
 # get derived variables at each pressure level
-deformation,convergence = windProducts(lons,lats,uwind,vwind)
-vws = windShear(uwind,vwind,hght)
-ellrod = (vws * (deformation + convergence)) * 1e7
-wspd = windSpeed(uwind,vwind) * MPS_TO_KT
-theta = potentialTemperature(temp,temp_pres)
-ri = richardsonNumber(theta,hght,vws)
+deformation_now,convergence_now = windProducts(lons,lats,uwind_now,vwind_now)
+deformation_fcst,convergence_fcst = windProducts(lons,lats,uwind_fcst,vwind_fcst)
+vws_now = windShear(uwind_now,vwind_now,hght_now)
+ellrod_now = (vws_now * (deformation_now + convergence_now)) * 1e7
+wspd_now = windSpeed(uwind_now,vwind_now) * MPS_TO_KT
+theta_now = potentialTemperature(temp_now,temp_pres_now)
+ri_now = richardsonNumber(theta_now,hght_now,vws_now)
+div_tend = (-convergence_fcst + convergence_now) / (3600*3)
 
 # create the plots of wind speed and Ellrod Index
-for i in range(wspd.shape[0]):
-    if debug and uwind_pres[i] != 300:
+for i in range(wspd_now.shape[0]):
+    if debug and uwind_pres_now[i] != 300:
         continue
     else:
-        print('%.0f mb pressure level' % uwind_pres[i])
-        contourPlot(wspd[i,:,:],ellrod[i,:,:],uwind_pres[i],'Wind Speed and Ellrod Index','ellrod')
-        contourPlot(wspd[i,:,:],ri[i,:,:],uwind_pres[i],'Wind Speed and Richardson Number','ri')
+        print('%.0f mb pressure level' % uwind_pres_now[i])
+        contourPlot(wspd_now[i,:,:],ellrod_now[i,:,:],uwind_pres_now[i],'Wind Speed and Ellrod Index','ellrod')
+        contourPlot(wspd_now[i,:,:],ri_now[i,:,:],uwind_pres_now[i],'Wind Speed and Richardson Number','ri')
+        contourPlot(wspd_now[i,:,:],div_tend[i,:,:]*1e9,uwind_pres_now[i],'Wind Speed and Divergence Tendency','dt')
 
 print('Done')
